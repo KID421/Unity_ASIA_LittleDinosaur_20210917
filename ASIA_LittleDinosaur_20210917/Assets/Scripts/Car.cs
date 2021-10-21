014f84af2c242b0cf2e894a2903402776b5d0233
KID <50666207+KID421@users.noreply.github.com>
2021-10-21 22:09:26 +0800

    Update 欄位與方法

--- ASIA_LittleDinosaur_20210917/Assets/Scripts/Car.cs
+++ ASIA_LittleDinosaur_20210917/Assets/Scripts/Car.cs
@@ -8,6 +8,7 @@ using UnityEngine;      // 引用 Unity 引擎命名空間：可以使用此空間的 API
 // 括號皆是成對出現：()、[]、{}、<>、''、""
 public class Car : MonoBehaviour
 {
+    #region 欄位語法及四大常用類型
     // 欄位 Field：保存各種類型的資料
     // 語法：修飾詞 資料類型 欄位名稱 指定 預設值 結束符號
     // ※ 四大常用類型：
@@ -31,4 +32,87 @@ public class Car : MonoBehaviour
     public string brand = "賓士";
     [Header("是否有天窗")]
     public bool hasSkyWindow = true;
+    #endregion
+
+    #region Unity 內資料類型
+    // 顏色、向量 (座標)、按鍵、遊戲物件、元件
+    // 顏色 Color
+    public Color color;
+    public Color colorRed = Color.red;
+    public Color colorYellow = Color.yellow;
+    public Color colorCustom1 = new Color(0, 0, 1);
+    public Color colorCustom2 = new Color(1, 0, 1, 0.5f);
+    // 向量 2 ~ 4 Vector
+    public Vector2 v2;
+    public Vector2 v2One = Vector2.one;
+    public Vector2 v2Right = Vector2.right;
+    public Vector2 v2Left = Vector2.left;
+    public Vector2 v2Custom = new Vector2(1, 2);
+    public Vector3 v3Custom = new Vector3(1, 2, 3);
+    public Vector4 v4Custom = new Vector4(1, 2, 3, 4);
+    // 按鍵 Keycode
+    public KeyCode keycode;
+    public KeyCode keycodeMouseLeft = KeyCode.Mouse0;
+    public KeyCode keycodeJump = KeyCode.Space;
+    // 遊戲物件 GameObject
+    public GameObject goCamera;
+    public GameObject goCar;
+    // 元件 Component
+    public Transform traCamera;
+    public Camera cam;
+    public SpriteRenderer spr;
+    #endregion
+
+    #region 事件：程式的入口
+    // 開始事件：遊戲播放時執行一次
+    private void Start()
+    {
+        // 呼叫方法：方法名稱();
+        Test();
+        Drive80();
+        Drive100();
+        Drive(120);     // 呼叫時填入的稱為：引數
+        Drive(990);
+        float w99 = ConvertWeight(9.9f);
+        print("9.9 轉換：" + w99);
+
+        print("重量轉換：" + ConvertWeight(weight));
+    }
+    #endregion
+
+    #region 方法語法
+    // 方法：保存程式內容的區塊，演算法、陳述式
+    // 語法：修飾詞 傳回類型 方法名稱 (參數1，參數2，...，參數N) { 程式內容 }
+    // void 無傳回：使用方法時不會有傳回資料
+    // 自訂方法：不會執行，必須呼叫才會執行
+    private void Test()
+    {
+        // 輸出(訊息) - 將訊息輸出至 Unity 儀錶板 Console
+        print("哈囉，沃德 :D");
+    }
+
+    private void Drive80()
+    {
+        print("開車時速：" + 80);
+        print("音效：咻咻咻");
+    }
+
+    private void Drive100()
+    {
+        print("開車時速：" + 100);
+        print("音效：咻咻咻");
+    }
+
+    // 參數語法：資料類型 參數名稱
+    private void Drive(int speed)
+    {
+        print("開車時速：" + speed);
+        print("音效：咻咻咻");
+    }
+
+    private float ConvertWeight(float setWeight)
+    {
+        return setWeight * 50;
+    }
+    #endregion
 }
