5d930c663ae8418676c514d9a23db55ea53fa3e3
KID421 <50666207+KID421@users.noreply.github.com>
2021-12-03 15:18:09 +0800

    Update 完成控制器系統並添加動畫參數與元件欄位

--- ASIA_LittleDinosaur_20210917/Assets/Scripts/Controller2D.cs
+++ ASIA_LittleDinosaur_20210917/Assets/Scripts/Controller2D.cs
@@ -17,13 +17,26 @@ public class Controller2D : MonoBehaviour
     [Header("跳躍按鍵與可跳躍圖層")]
     public KeyCode keyJump = KeyCode.Space;
     public LayerMask canJumpLayer;
+    [Header("動畫參數：走路與跳躍")]
+    public string parameterWalk = "開關走路";
+    public string parameterJump = "開關跳躍";
     #endregion
 
+    #region 欄位：私人
+    private Animator ani;
     /// <summary>
     /// 剛體元件 Rigidbody 2D
     /// </summary>
     private Rigidbody2D rig;
+    // 將私人欄位顯示在屬性面板上
+    [SerializeField]
+    /// <summary>
+    /// 是否在地板上
+    /// </summary>
+    private bool isGrounded;
+    #endregion
 
+    #region 事件
     /// <summary>
     /// 繪製圖示
     /// 在 Unity 繪製輔助用的圖示
@@ -45,6 +58,7 @@ public class Controller2D : MonoBehaviour
     {
         // 剛體欄位 = 取得元件<2D 剛體>()
         rig = GetComponent<Rigidbody2D>();
+        ani = GetComponent<Animator>();
     }
 
     /// <summary>
@@ -60,7 +74,10 @@ public class Controller2D : MonoBehaviour
     private void Update()
     {
         Flip();
+        CheckGround();
+        Jump();
     }
+    #endregion
 
     #region 方法
     /// <summary>
@@ -71,7 +88,7 @@ public class Controller2D : MonoBehaviour
     {
         // h 值 指定為 輸入.取得軸向(水平軸) - 水平軸代表左右鍵與 AD
         float h = Input.GetAxis("Horizontal");
-        print("玩家左右按鍵值：" + h);
+        // print("玩家左右按鍵值：" + h);
 
         // 剛體元件.加速度 = 新 二維向量(h 值 * 移動速度，剛體.加速度.垂直)；
         rig.velocity = new Vector2(h * speed, rig.velocity.y);
@@ -95,6 +112,33 @@ public class Controller2D : MonoBehaviour
         else if (h > 0)
         {
             transform.eulerAngles = Vector3.zero;
+        }
+    }
+
+    /// <summary>
+    /// 檢查是否在地板
+    /// </summary>
+    private void CheckGround()
+    {
+        // 碰撞資訊 = 2D 物理.覆蓋圓形(中心點，半徑，圖層)
+        Collider2D hit = Physics2D.OverlapCircle(transform.position +
+            transform.TransformDirection(checkGroundOffset), checkGroundRadius, canJumpLayer);
+
+        // print("碰到的物件名稱：" + hit.name);
+
+        isGrounded = hit;
+    }
+
+    /// <summary>
+    /// 跳躍
+    /// </summary>
+    private void Jump()
+    {
+        // 如果 在地板上 並且 按下指定按鍵
+        if (isGrounded && Input.GetKeyDown(keyJump))
+        {
+            // 剛體.添加推力(二維向量)
+            rig.AddForce(new Vector2(0, jump));
         }
     }
     #endregion
