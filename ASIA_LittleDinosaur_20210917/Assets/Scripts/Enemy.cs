f5ebac1daefd9f485b4350c2f2c4b7ed1c099b27
KID421 <50666207+KID421@users.noreply.github.com>
2021-12-24 14:02:54 +0800

    Update 繪製追蹤區域

--- ASIA_LittleDinosaur_20210917/Assets/Scripts/Enemy.cs
+++ ASIA_LittleDinosaur_20210917/Assets/Scripts/Enemy.cs
@@ -16,4 +16,25 @@ public class Enemy : MonoBehaviour
     [Header("目標圖層")]
     public LayerMask layerTarget;
     #endregion
+
+    #region 事件
+    private void OnDrawGizmos()
+    {
+        // 指定圖示的顏色
+        Gizmos.color = new Color(1, 0, 0, 0.3f);
+        // 繪製立方體(中心，尺寸)
+        Gizmos.DrawCube(transform.position + transform.TransformDirection(v3TrackOffset), v3TrackSize);
+    }
+    #endregion
+
+    #region 方法
+    /// <summary>
+    /// 檢查目標是否在區域內
+    /// </summary>
+    private void CheckTargetInArea()
+    {
+        // 2D 物理.覆蓋盒形(中心，尺寸，角度)
+        Physics2D.OverlapBox(transform.position + transform.TransformDirection(v3TrackOffset), v3TrackSize, 0);
+    }
+    #endregion
 }
