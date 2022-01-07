677ea7a3f46408a34ff4a2d00ced04692b57af7a
KID421 <50666207+KID421@users.noreply.github.com>
2022-01-07 15:50:26 +0800

    Update 完成敵人 AI、受傷並添加失敗與過關

--- ASIA_LittleDinosaur_20210917/Assets/Scripts/Enemy.cs
+++ ASIA_LittleDinosaur_20210917/Assets/Scripts/Enemy.cs
@@ -11,18 +11,27 @@ public class Enemy : MonoBehaviour
     [Header("檢查追蹤區域大小與位移")]
     public Vector3 v3TrackSize = Vector3.one;
     public Vector3 v3TrackOffset;
-    [Header("移動速度")]
+    [Header("移動速度"), Range(0, 10)]
     public float speed = 1.5f;
     [Header("目標圖層")]
     public LayerMask layerTarget;
     [Header("動畫參數")]
     public string parameterWalk = "開關走路";
+    public string parameterAttack = "觸發攻擊";
     [Header("面向目標物件")]
     public Transform target;
+    [Header("攻擊距離"), Range(0, 5)]
+    public float attackDistance = 1.3f;
+    [Header("攻擊冷卻時間"), Range(0, 10)]
+    public float attackCD = 2.8f;
+    [Header("檢查攻擊區域大小與位移")]
+    public Vector3 v3AttackSize = Vector3.one;
+    public Vector3 v3AttackOffset;
 
     private float angle = 0;
     private Rigidbody2D rig;
     private Animator ani;
+    private float timerAttack;
     #endregion
 
     #region 事件
@@ -38,6 +47,9 @@ public class Enemy : MonoBehaviour
         Gizmos.color = new Color(1, 0, 0, 0.3f);
         // 繪製立方體(中心，尺寸)
         Gizmos.DrawCube(transform.position + transform.TransformDirection(v3TrackOffset), v3TrackSize);
+
+        Gizmos.color = new Color(0, 1, 0, 0.3f);
+        Gizmos.DrawCube(transform.position + transform.TransformDirection(v3AttackOffset), v3AttackSize);
     }
 
     private void Update()
@@ -63,6 +75,7 @@ public class Enemy : MonoBehaviour
     /// </summary>
     private void Move()
     {
+        #region 使用判斷式 if 與三源運算子的對照
         // 如果 目標的 X 小於 敵人的 X 就代表在左邊 角度 0
         // 如果 目標的 X 大於 敵人的 X 就代表在右邊 角度 180
         if (target.position.x > transform.position.x)
@@ -73,7 +86,9 @@ public class Enemy : MonoBehaviour
         {
             // 左邊 angle = 0;
         }
+        #endregion
 
+        #region 移動
         // 三元運算子語法：布林值 ? 當布林值 為 true : 當布林值 為 false ;
         angle = target.position.x > transform.position.x ? 180 : 0;
         // 變形.歐拉角度 = Y * 角度
@@ -84,7 +99,35 @@ public class Enemy : MonoBehaviour
 
         // 距離 = 三維向量.距離(A點，B點)
         float distance = Vector3.Distance(target.position, transform.position);
-        print("與目標的距離：" + distance);
+        // print("與目標的距離：" + distance);
+        #endregion
+
+        if (distance <= attackDistance)     // 如果 距離 小於等於 攻擊距離
+        {
+            rig.velocity = Vector3.zero;    // 停止
+            Attack();
+        }
+    }
+
+    [Header("攻擊力"), Range(0, 100)]
+    public float attack = 35;
+
+    /// <summary>
+    /// 攻擊
+    /// </summary>
+    private void Attack()
+    {
+        if (timerAttack < attackCD)                 // 如果 計時器 小於 冷卻時間
+        {
+            timerAttack += Time.deltaTime;          // 時間累加 Time.deltaTime 一幀的時間
+        }
+        else
+        {
+            ani.SetTrigger(parameterAttack);        // 如果 計時器 大於等於 冷卻時間 就 攻擊
+            timerAttack = 0;                        // 計時器 歸零
+            Collider2D hit = Physics2D.OverlapBox(transform.position + transform.TransformDirection(v3AttackOffset), v3AttackSize, 0, layerTarget);
+            hit.GetComponent<HurtSystem>().Hurt(attack);
+        }
     }
     #endregion
 }
